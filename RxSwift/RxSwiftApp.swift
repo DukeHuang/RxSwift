4b1afc10ba613ec79c37bcad916516bc2dce3a38
sakuragi <378387959@qq.com>
2022-05-28 09:05:54 +0800

    configuration file with project

---
+++ RxSwift/RxSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  RxSwiftApp.swift
+//  RxSwift
+//
+//  Created by yongyou on 2022/5/27.
+//
+
+import SwiftUI
+
+@main
+struct RxSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
